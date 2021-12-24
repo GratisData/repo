dd8b3c6ee7c86e9f16cc0a5aeed7a5f2382e98eb
Martin Carlsson <martin@imus.dk>
2021-12-24 11:36:46 +0000

    Install dagit

---
+++ demo_repo/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='demo_repo',
+      version='0.1',
+      description='Test of a Dagster Repo',
+      url='https://github.com/GratisData/repo',
+      author='Martin Carlsson',
+      author_email='martin@imus.dk',
+      license='MIT',
+      packages=['demo_repo'],
+      zip_safe=False)
